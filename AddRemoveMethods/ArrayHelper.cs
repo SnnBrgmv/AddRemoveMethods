8a7b64a2fce2f4909e20fad2ce329c4a2699b2b1
SnnBrgmv <sananai@code.edu.az>
2023-08-30 17:05:30 +0400

    Add and Remove methods of Array

---
+++ AddRemoveMethods/ArrayHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddRemoveMethods
+{
+    public class ArrayHelper
+    {
+        public static string[] Add(ref string[] arr, string value)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = value;
+            return arr;
+        }
+        public static string[] Remove(ref string[] arr, string value)
+        {
+            int removeIndex = Array.IndexOf(arr, value);
+            if (removeIndex == -1)
+            {
+                Console.WriteLine("Value is not found in Array");
+            }
+
+            for (int i = removeIndex; i < arr.Length - 1; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+
+            Array.Resize(ref arr, arr.Length - 1);
+
+            return arr;
+        }
+
+    }
+}
---
+++ AddRemoveMethods/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace AddRemoveMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nameList = new string[2] { "Rufat", "Nicat" };
+
+            Console.WriteLine("Original Array:");
+            foreach (var name in nameList)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine($"Original Array Length: {nameList.Length}");
+
+            Console.WriteLine("-------------------------------------------------");
+
+            ArrayHelper.Add(ref nameList, "Vasif");
+            ArrayHelper.Add(ref nameList, "Vusal");
+            ArrayHelper.Add(ref nameList, "Elekber");
+
+            Console.WriteLine("After Adding:");
+            foreach (var name in nameList)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine($"After Adding Array Length: {nameList.Length}");
+
+            Console.WriteLine("-------------------------------------------------");
+
+            ArrayHelper.Remove(ref nameList, "Vasif");
+            ArrayHelper.Remove(ref nameList, "Rufat");
+            ArrayHelper.Remove(ref nameList, "Nicat");
+            ArrayHelper.Remove(ref nameList, "Vusal");
+
+
+
+            Console.WriteLine("After Removing:");
+            foreach (var name in nameList)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine($"After Removing Array Length: {nameList.Length}");
+        }
+    }
+}
